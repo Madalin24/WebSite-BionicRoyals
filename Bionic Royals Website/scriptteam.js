04dd32e2736170efffe0b331bad0a16d74b8cfd0
Madalin.K <madalinmiahi.mitulescu@gmail.com>
2026-03-16 21:13:14 +0200

    Beta 0.4.3

--- Bionic Royals Website/scriptteam.js
+++
@@ -1,9 +0,0 @@
-// --- HAMBURGER MENU LOGIC ---
-
-function toggleMenu() {
-    // Finds the dropdown menu in your HTML
-    const menu = document.getElementById("dropdownMenu");
-    
-    // Toggles the "show" class on and off!
-    menu.classList.toggle("show");
-}
